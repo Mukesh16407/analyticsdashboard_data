e120527be3b203f1de7891aee7838af782db8e05
Mukesh16407 <mukesh.16407@gmail.com>
2024-04-04 11:23:53 +0530

    change version json-server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
+console.log(server);
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
